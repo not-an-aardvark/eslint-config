c7f43c5b4d4f48ba8b931f0e6332ea5e4158d994
Teddy Katz <teddy.katz@gmail.com>
2016-11-02 18:04:22 -0400

    Chore: add tests for commit messages

--- test/build.js
+++ test/build.js
@@ -2,11 +2,35 @@
 
 const path = require('path');
 const fs = require('fs');
+const childProcess = require('child_process');
 const expect = require('chai').use(require('dirty-chai')).expect;
 
+function exec (command) {
+  return childProcess.execSync(command).toString();
+}
+
+function getLatestCommitMessage () {
+  return exec('git --no-pager show -s --oneline --format="%B" HEAD');
+}
+
 describe('build', () => {
-  it('points to a file that exists', () => {
-    const mainFile = path.join(__dirname, '..', require('../package.json').main);
-    expect(fs.existsSync(mainFile)).to.be.true(`The main file ${mainFile} does not exist.`);
+  describe('package.json', () => {
+    it('points to a file that exists', () => {
+      const mainFile = path.join(__dirname, '..', require('../package.json').main);
+      expect(fs.existsSync(mainFile)).to.be.true(`The main file ${mainFile} does not exist.`);
+    });
+  });
+  describe('commit message', () => {
+    it('has at most 72 characters', () => {
+      const latestCommit = getLatestCommitMessage();
+      expect(latestCommit.length).to.be.at.most(72, 'Commit message is too long; must be at most 72 characters.');
+    });
+    it('starts with a valid prefix', () => {
+      const latestCommit = getLatestCommitMessage();
+      const PREFIXES = ['Fix:', 'Update:', 'New:', 'Breaking:', 'Docs:', 'Build:', 'Upgrade:', 'Chore:'];
+      expect(
+        PREFIXES.some(prefix => latestCommit.startsWith(prefix))
+      ).to.be.true(`Commit message must start with one of ${PREFIXES.join(', ')}`);
+    });
   });
 });
