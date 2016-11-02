6f483a91c3f07231bb169ab137b5179da56e95a1
Teddy Katz <teddy.katz@gmail.com>
2016-11-02 18:11:32 -0400

    Chore: move lib files out of root directory

--- tests/rules.js
+++ tests/rules.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const fs = require('fs');
 const expect = require('chai').use(require('dirty-chai')).expect;
-const config = require('../es2015');
+const config = require('../lib/es2015');
 const configValidator = require('eslint/lib/config/config-validator');
 const RULES_DIR = path.join(__dirname, '../node_modules/eslint/lib/rules/');
 const CORE_RULES = fs.readdirSync(RULES_DIR)
